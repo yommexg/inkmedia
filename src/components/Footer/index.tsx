3e6afae82c6806c367e1794b9a931b8d29ac873c
Bolu Yomi-olugbodi  (Anime Code Lord) <98047628+yommexg@users.noreply.github.com>
2024-10-15 21:53:21 +0100

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,8 +9,8 @@ const Footer: React.FC = () => {
   ];
 
   return (
-    <footer className=" py-4 flex flex-col items-center bg-[#FFD700]">
-      <div className="flex flex-col  md:flex-row gap-2 justify-center items-center">
+    <footer className="py-4 flex flex-col items-center bg-[#FFD700]">
+      <div className="flex flex-col md:flex-row gap-2 justify-center items-center">
         <p className="text-sm font-bold">
           © 2024 InksMedia Creatives. All rights reserved.
         </p>
@@ -23,15 +23,15 @@ const Footer: React.FC = () => {
                 rel="noopener noreferrer"
                 className="text-red-500 hover:text-white transition duration-300 ease-in-out"
               >
-                <link.icon size={24} />
+                {React.createElement(link.icon, { size: 24 })}
               </a>
             </li>
           ))}
         </ul>
       </div>
-    
     </footer>
   );
 };
 
 export default Footer;
+
